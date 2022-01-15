ea8455f3456074d2752608104fb8abd1ebb2c6ea
Robert D. Rioja (miapuffia) <miapuffia@gmail.com>
2022-01-15 04:20:47 -0500

    Added fix for some source files failing to queue

--- CompressAllVideos.bat
+++ CompressAllVideos.bat
@@ -1,7 +1,7 @@
 :: -crf can be 19 for higher quality at double the size
 
 for %%f in (*.mp4) do (
-	ffmpeg -i "%%f" -c:v libx264 -crf 24 -level 3.1 -preset veryslow "%%~nf.tiny.mp4"
+	ffmpeg -i "%%f" -c:v libx264 -crf 24 -level 3.1 -preset veryslow -max_muxing_queue_size 9999 "%%~nf.tiny.mp4"
 )
 
 pause
